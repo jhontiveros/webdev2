fe145441641d6414aec040bf9c754c4c71c0243b
jhontiveros <jennicahontiveros@gmail.com>
2022-03-24 21:08:38 -0500

    finishing kolory :)

---
+++ projects/midterm-sites/kolory/script.js
@@ -0,0 +1,13 @@
+function onAboutClicked() {
+    document.getElementById("about").classList.remove("hidden");
+  }
+  
+  function onContactClicked() {
+    document.getElementById("contact").classList.remove("hidden");
+  }
+  
+  function onXClicked() {
+    document.getElementById("about").classList.add("hidden");
+    document.getElementById("contact").classList.add("hidden");
+  }
+  
