a41f2187536cf04ec95bbc412eb248797107475f
jhontiveros <jennicahontiveros@gmail.com>
2022-05-06 02:57:52 -0500

    adding bg music and album specific

--- projects/final-project/script.js
+++ projects/final-project/script.js
@@ -1,3 +1,5 @@
+var augh = null
+
 function calcDynamicHeight(ref) {
     const vw = window.innerWidth
     const vh = window.innerHeight
@@ -5,7 +7,23 @@ function calcDynamicHeight(ref) {
     return objectWidth - vw + vh + 150 // ul margin-right
 }
 
+function onAlbumClick(audioId) {
+    // stop all audio + stop all spinning vinyls
+    augh.pause(); augh.currentTime = 0;
+    document.getElementById("aughVinyl").classList.remove("rotate")
+
+    // play the music
+    const a = document.getElementById(audioId)
+    a.volume = 0.3
+    a.play()
+
+    // spin the vinyl
+    document.getElementById(audioId + "Vinyl").classList.add("rotate")
+}
+
 document.addEventListener("DOMContentLoaded", function(event) { 
+    augh = document.getElementById('augh')
+
     const pContainer = document.querySelector('.p-container')
     const horizontal = document.querySelector('.horizontal')
     pContainer.style.height = `${calcDynamicHeight(horizontal)}px`
@@ -18,4 +36,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     window.addEventListener('resize', () => {
         pContainer.style.height = `${calcDynamicHeight(horizontal)}px`
     })
+
+    // audio changes
+    document.getElementById("bgm").volume = 0.6;
 });
