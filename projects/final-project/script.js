c25d939b02ec8a724ce5f3c7442ddc0cfa52d0d3
jhontiveros <jennicahontiveros@gmail.com>
2022-05-06 01:47:02 -0500

    album animations

--- projects/final-project/script.js
+++ projects/final-project/script.js
@@ -3,13 +3,12 @@ function calcDynamicHeight(ref) {
     const vh = window.innerHeight
     const objectWidth = ref.scrollWidth
     return objectWidth - vw + vh + 150 // ul margin-right
-} 
+}
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     const pContainer = document.querySelector('.p-container')
     const horizontal = document.querySelector('.horizontal')
     pContainer.style.height = `${calcDynamicHeight(horizontal)}px`
-
 
     window.addEventListener('scroll', () => {
         const sticky = document.querySelector('.sticky')
